9be367b9109a42c17b1b2b25598f874b48b45c9d
IlanYagutilov3000 <ilanyagotilov191@gmail.com>
2024-10-24 14:57:17 +0300

    new upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  let [artist, setArtist] = useState<string>("");
+  let [song, setSong] = useState<string>("");
+  let [lyrics, setLyrics] = useState<string>("");
+
+  function searchLyrics() {
+    if (artist === "" || song === "") {
+      alert("Please write something")
+      return;
+    }
+    axios.get(
+      `https://api.lyrics.ovh/v1/${artist}/${song}`).then(res => {
+        setLyrics(res.data.lyrics)
+        setArtist("");
+        setSong("");
+      }).catch((err) => console.log(err)
+      );
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lyric Finder</h1>
+      <input type="text" placeholder='Artist Name' onChange={(e) => { setArtist(e.target.value) }} value={artist} />
+      <input type="text" placeholder='Song Name' onChange={(e) => { setSong(e.target.value) }} value={song} />
+      <button onClick={() => searchLyrics()}>Serach</button>
+      <pre>{lyrics}</pre>
     </div>
   );
 }
